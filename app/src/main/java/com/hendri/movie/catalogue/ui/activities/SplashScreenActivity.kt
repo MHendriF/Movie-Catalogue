183f3fe22ce7951e6af1b7653f1b86e8f84fe64b
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-11-08 12:22:47 +0700

    section pager done

--- app/src/main/java/com/hendri/movie/catalogue/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/hendri/movie/catalogue/ui/activities/SplashScreenActivity.kt
@@ -1,21 +1,20 @@
 package com.hendri.movie.catalogue.ui.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.hendri.movie.catalogue.R
 
 class SplashScreenActivity : AppCompatActivity() {
-    lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        handler = Handler()
-        handler.postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
