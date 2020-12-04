475d9e4c14cdf3437a0fe7f9a1a7d99b5b7f9efe
MHendriF <hendrifebriansyah28@gmail.com>
2020-12-04 23:29:31 +0700

    show data on detail activity ok

--- app/src/main/java/com/hendri/movie/catalogue/ui/fragments/DetailMovieFragment.kt
+++ app/src/main/java/com/hendri/movie/catalogue/ui/fragments/DetailMovieFragment.kt
@@ -1,7 +1,10 @@
 package com.hendri.movie.catalogue.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
+import androidx.databinding.BindingAdapter
 import androidx.fragment.app.activityViewModels
 import com.hendri.movie.catalogue.R
 import com.hendri.movie.catalogue.base.BaseFragment
@@ -18,12 +21,11 @@ class DetailMovieFragment : BaseFragment<FragmentDetailMovieBinding>() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
+        binding.ivBack.setOnClickListener { activity?.onBackPressed() }
         viewModel.dataMovie?.observe(viewLifecycleOwner, { handleStat(it) })
     }
 
     private fun handleStat(resource: Resource<MovieDetailResponse>) {
-
         when (resource) {
             is Resource.Loading -> binding.isLoading = true
             is Resource.Empty -> binding.isLoading = false
@@ -31,12 +33,27 @@ class DetailMovieFragment : BaseFragment<FragmentDetailMovieBinding>() {
                 binding.isLoading = false
                 resource.data.let { data ->
                     binding.model = data
+                    binding.tvReadMore.setOnClickListener {
+                        if (binding.tvReadMore.text.toString() == "Read More") {
+                            binding.tvOverview.maxLines = Int.MAX_VALUE
+                            binding.tvOverview.ellipsize = null
+                            binding.tvReadMore.setText(R.string.read_less)
+                        } else {
+                            binding.tvOverview.maxLines = 4
+                            binding.tvOverview.ellipsize = TextUtils.TruncateAt.END
+                            binding.tvReadMore.setText(R.string.read_more)
+                        }
+                    }
                 }
             }
             is Resource.Error -> {
                 binding.isLoading = false
-                Toast.makeText(requireContext(), resource.errorMessage, Toast.LENGTH_SHORT).show()
+                activity?.toast(resource.errorMessage)
             }
         }
+    }
+
+    private fun Context.toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/hendri/movie/catalogue/ui/fragments/DetailTvShowFragment.kt
+++ app/src/main/java/com/hendri/movie/catalogue/ui/fragments/DetailTvShowFragment.kt
@@ -1,6 +1,8 @@
 package com.hendri.movie.catalogue.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.hendri.movie.catalogue.R
@@ -19,6 +21,7 @@ class DetailTvShowFragment : BaseFragment<FragmentDetailTvShowBinding>() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        binding.ivBack.setOnClickListener { activity?.onBackPressed() }
         viewModel.dataTv?.observe(viewLifecycleOwner, { handleStat(it) })
     }
 
@@ -31,12 +34,27 @@ class DetailTvShowFragment : BaseFragment<FragmentDetailTvShowBinding>() {
                 binding.isLoading = false
                 resource.data.let { data ->
                     binding.model = data
+                    binding.tvReadMore.setOnClickListener {
+                        if (binding.tvReadMore.text.toString() == "Read More") {
+                            binding.tvOverview.maxLines = Int.MAX_VALUE
+                            binding.tvOverview.ellipsize = null
+                            binding.tvReadMore.setText(R.string.read_less)
+                        } else {
+                            binding.tvOverview.maxLines = 4
+                            binding.tvOverview.ellipsize = TextUtils.TruncateAt.END
+                            binding.tvReadMore.setText(R.string.read_more)
+                        }
+                    }
                 }
             }
             is Resource.Error -> {
                 binding.isLoading = false
-                Toast.makeText(requireContext(), resource.errorMessage, Toast.LENGTH_SHORT).show()
+                activity?.toast(resource.errorMessage)
             }
         }
+    }
+
+    private fun Context.toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
